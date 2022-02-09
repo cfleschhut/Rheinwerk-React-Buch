d64940901605082019597acb2fe63345ae916a8d
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-02-09 18:07:59 +0100

    Add and display Animal class data

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,40 +1,55 @@
 import React from 'react';
 import './Card.css';
+import Animal from './Animal';
+
+const uncovered = false;
 
 export default function Card() {
-  return (
+  const elephant = new Animal(
+    'Elefant',
+    'placeholder.png',
+    3.3,
+    6000,
+    70,
+    1,
+    40,
+  );
+
+  const front = (
     <div className="card">
-      <h1>Elefant</h1>
-      <img
-        alt="Elefant"
-        src={`${process.env.PUBLIC_URL}/placeholder.png`}
-        height="200"
-        width="200"
-      />
+      <h1>{elephant.name ? elephant.name : 'Unbekannt'}</h1>
+      {elephant.image && (
+        <img
+          alt="Elefant"
+          src={`${process.env.PUBLIC_URL}/${elephant.image}`}
+          height="200"
+          width="200"
+        />
+      )}
       <table>
         <tbody>
-          <tr>
-            <td>Größe</td>
-            <td>3.3 m</td>
-          </tr>
-          <tr>
-            <td>Gewicht</td>
-            <td>6000 kg</td>
-          </tr>
-          <tr>
-            <td>Alter</td>
-            <td>70 Jahre</td>
-          </tr>
-          <tr>
-            <td>Nachkommen</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>Geschwindigkeit</td>
-            <td>40 km/h</td>
-          </tr>
+          {Object.keys(Animal.properties).map((property) => {
+            const animalProperty = Animal.properties[property];
+            return (
+              <tr key={property}>
+                <td>{animalProperty.label}</td>
+                <td>
+                  {elephant[property]}
+                  {animalProperty.unit}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
   );
+
+  const back = <div className="card back" />;
+
+  if (uncovered) {
+    return front;
+  }
+
+  return back;
 }
