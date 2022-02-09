1a5a9a449fcb99b853b27a156339a5f0a93d7434
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-02-10 00:31:57 +0100

    Finalize initial game logic

--- src/Game.jsx
+++ src/Game.jsx
@@ -13,22 +13,91 @@ export default class Game extends Component {
       computerUncovered: false,
       selectedProperty: '',
       playersTurn: true,
-      player: [new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40)],
+      player: [
+        new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40),
+        new Animal('Flusspferd', 'placeholder.png', 1.5, 1800, 50, 1, 30),
+      ],
       computer: [
-        new Animal('Nashorn', 'placeholder.png', 1, 9, 2300, 50, 1, 50),
+        new Animal('Nashorn', 'placeholder.png', 1.9, 2300, 50, 1, 50),
+        new Animal('Krokodil', 'placeholder.png', 5.2, 1000, 70, 60, 29),
       ],
     };
   }
 
   getSelectPropertyHandler() {
-    return (property) => {
-      this.setState((state) =>
+    return (property) => this.play(property);
+  }
+
+  compare(property) {
+    let { playersTurn } = this.state;
+
+    const firstPlayer = this.state.player[0];
+    let player = update(this.state.player, { $splice: [[0, 1]] });
+    const firstComputer = this.state.computer[0];
+    let computer = update(this.state.computer, { $splice: [[0, 1]] });
+
+    if (firstPlayer[property] > firstComputer[property]) {
+      playersTurn = true;
+      player = update(player, { $push: [firstPlayer, firstComputer] });
+
+      if (computer.length === 0) {
+        alert('Player wins');
+        return;
+      }
+    } else if (firstPlayer[property] < firstComputer[property]) {
+      playersTurn = false;
+      computer = update(computer, { $push: [firstPlayer, firstComputer] });
+
+      if (player.length === 0) {
+        alert('Computer wins');
+        return;
+      }
+    } else {
+      player = update(player, { $push: [firstPlayer] });
+      computer = update(computer, { $push: [firstComputer] });
+    }
+
+    this.setState(
+      (state) =>
+        update(state, {
+          $set: {
+            computerUncovered: false,
+            selectedProperty: '',
+            playersTurn,
+            player,
+            computer,
+          },
+        }),
+      () => {
+        if (!playersTurn) {
+          setTimeout(() => {
+            const property = this.selectRandomProperty();
+            this.play(property);
+          }, 2000);
+        }
+      },
+    );
+  }
+
+  play(property) {
+    this.setState(
+      (state) =>
         update(state, {
           selectedProperty: { $set: property },
           computerUncovered: { $set: true },
         }),
-      );
-    };
+      () => {
+        setTimeout(() => {
+          this.compare(property);
+        }, 2000);
+      },
+    );
+  }
+
+  selectRandomProperty() {
+    const properties = Object.keys(Animal.properties);
+    const index = Math.floor(Math.random() * properties.length);
+    return properties[index];
   }
 
   render() {
