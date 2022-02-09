3d80075674ccc60550d0fc9d59b6fb169f6b835d
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-02-09 15:11:44 +0100

    Add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Card from './Card';
 import './App.css';
 
 function App() {
-  return <h1>Supertrumpf</h1>;
+  return (
+    <div>
+      <h1>Supertrumpf</h1>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
