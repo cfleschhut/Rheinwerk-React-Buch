83897a65977a3371641a7391b19b603b0a121e1a
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-02-16 01:15:17 +0100

    Add error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Game from './Game.container';
 // import LifecycleExample from './LifecycleExample';
 // import HigherOrderComponentExample from './HigherOrderComponentExample';
@@ -6,14 +6,46 @@ import Game from './Game.container';
 import DealCards from './DealCards';
 import './App.css';
 
-export default function App() {
-  return (
-    <>
+class ErrorBoundary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      error: null,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      error: error.message,
+    };
+  }
+
+  render() {
+    const { error } = this.state;
+
+    if (error) {
+      return <div>{error}</div>;
+    }
+
+    return (
       <DealCards>
         {(player, computer) => (
           <Game title="Supertrumpf" player={player} computer={computer} />
         )}
       </DealCards>
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <>
+      <ErrorBoundary />
 
       {/* <LifecycleExample /> */}
       {/* <HigherOrderComponentExample /> */}
